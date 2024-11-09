fc4a2f56fe2ccf0a7185a282bbd13ebdc7cebdf8
DESKTOP-6VOT7A0\Piramid <dvdmr316@gmail.com>
2024-11-09 18:46:07 -0500

    Updates

---
+++ next/static/D75GWb38a8tYnO4NjhIsg/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/615-eed0a001c3c0c007.js","static/chunks/pages/index-b7e96819582c204c.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
